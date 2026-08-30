b6493b407a9c0235e64ca9296aeae29f5a0e8beb
Nafea Mostafa <nafeelkassas4@gmail.com>
2026-08-31 01:01:33 +0300

    created constants file with base url

---
+++ WinsomeTask/Resources/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  WinsomeTask
+//
+//  Created by Nafe3's Macbook on 31/08/2026.
+//
+
+import Foundation
+final class Constants {
+    static let shared = Constants()
+    private init() {}
+    
+    let baseURL = "https://dummyjson.com/products"
+}
