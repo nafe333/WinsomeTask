4e8b55ef7bcaa841743ab8a8b216c4d21bc19e4c
Nafea Mostafa <nafeelkassas4@gmail.com>
2026-08-30 22:55:58 +0300

    created folders for mvvm pattern

--- WinsomeTask/Core/HomeView/HomeView.swift
+++ WinsomeTask/Core/HomeView/HomeView.swift
@@ -7,18 +7,19 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct HomeView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+            
         }
         .padding()
     }
 }
 
 #Preview {
-    ContentView()
+    HomeView()
 }
