e0092b406d25e3399469b969584727ec9aab7b2b
Nafea Mostafa <nafeelkassas4@gmail.com>
2026-08-30 22:51:02 +0300

    Initial Commit

---
+++ WinsomeTask/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WinsomeTask
+//
+//  Created by Nafe3's Macbook on 30/08/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WinsomeTask/WinsomeTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  WinsomeTaskApp.swift
+//  WinsomeTask
+//
+//  Created by Nafe3's Macbook on 30/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct WinsomeTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
